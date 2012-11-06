ea27f5b765f69a15899abbca0c626bd090c1084e
Andrews Medina <andrewsmedina@gmail.com>
2012-11-06 17:43:54 -0200

    added license

--- tsuru/stream/__init__.py
+++ tsuru/stream/__init__.py
@@ -1,3 +1,7 @@
+# Copyright 2012 tsuru-circus authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 import json
 import os
 import re
@@ -8,6 +12,7 @@ def extract_message(msg):
     regex = "\d+\-\d+\-\d+ \d+\:\d+\:\d+ \[\d+\] \[\w+\] " #like 2012-11-06 18:30:10 [13887] [INFO]
     msgs = re.split(regex, msg)
     return [msg for msg in msgs if msg] #removing empty entries
+
 
 class Stream(object):
 
