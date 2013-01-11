1ad9441acc8c508fd1488ee9e02b7a1a153f8b13
Francisco Souza <f@souza.cc>
2013-01-11 10:16:13 -0200

    setup: bump to 0.1.2

--- tsuru/__init__.py
+++ tsuru/__init__.py
@@ -1,5 +1,5 @@
-# Copyright 2012 tsuru-circus authors. All rights reserved.
+# Copyright 2013 tsuru-circus authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
