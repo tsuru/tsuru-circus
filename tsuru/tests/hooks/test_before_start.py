b86d4966d0503a6679ca8d5db869b49a709f02f0
Andrews Medina <andrewsmedina@gmail.com>
2013-08-12 17:46:48 -0300

    simplified the before_start tests.

--- tsuru/tests/hooks/test_before_start.py
+++ tsuru/tests/hooks/test_before_start.py
@@ -3,27 +3,13 @@
 # license that can be found in the LICENSE file.
 
 from unittest import TestCase
-from mock import patch, call
+from mock import patch
 
 from tsuru.hooks import before_start
 
 
 class BeforeStartTest(TestCase):
-    @patch("tsuru.hooks.load_config")
-    @patch("os.system")
-    def test_before_start(self, system, load_config):
-        load_config.return_value = {
-            'pre-restart': ['testdata/pre.sh'],
-        }
+    @patch("tsuru.hooks.run_commands")
+    def test_before_start(self, run_commands):
         before_start()
-        system.assert_called_with("testdata/pre.sh")
-
-    @patch("tsuru.hooks.load_config")
-    @patch("os.system")
-    def test_before_start_many_commands(self, system, load_config):
-        load_config.return_value = {
-            'pre-restart': ['testdata/pre.sh', 'testdata/pre2.sh'],
-        }
-        before_start()
-        calls = [call("testdata/pre.sh"), call("testdata/pre2.sh")]
-        system.assert_has_calls(calls)
+        run_commands.assert_called_with('pre-restart')
