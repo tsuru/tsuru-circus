4efd18ebf733028f9b8c82a1fa8e80e22fe22a07
Francisco Souza <fss@corp.globo.com>
2014-07-17 14:56:19 -0300

    plugins: add pluging for reporting the status of the unit
    Related to #17.

--- tsuru/plugins/__init__.py
+++ tsuru/plugins/__init__.py
@@ -1,13 +1,16 @@
-# Copyright 2013 tsuru-circus authors. All rights reserved.
+# Copyright 2014 tsuru-circus authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
 import os
 import re
 
+from socket import gethostname
+
+import requests
+
 from circus.plugins import CircusPlugin
 from circus.client import CircusClient
-
 from honcho.procfile import Procfile
 from zmq.eventloop import ioloop
 
@@ -199,3 +202,45 @@ class ProcfileWatcher(CircusPlugin):
 
             for name, cmd in to_change.items():
                 self.change_cmd(name=name, cmd=procfile.commands[name])
+
+
+class StatusReporter(CircusPlugin):
+    name = "status_reporter"
+
+    def __init__(self, *args, **config):
+        super(StatusReporter, self).__init__(*args, **config)
+        self.loop_rate = config.get("loop_rate", 60)
+        self.apprc = config.get("apprc", "/home/application/apprc")
+        self.hostname = gethostname()
+        self.period = ioloop.PeriodicCallback(self.report,
+                                              self.loop_rate * 1000,
+                                              self.loop)
+
+    def handle_init(self):
+        self.period.start()
+
+    def handle_stop(self):
+        self.period.stop()
+
+    def handle_recv(self, data):
+        pass
+
+    def report(self):
+        cmds = self.call("status")["statuses"]
+        if "tsuru-hooks" in cmds:
+            del cmds["tsuru-hooks"]
+        status = "started"
+        for cmd_name, cmd_status in cmds.iteritems():
+            if cmd_name.startswith("plugin:"):
+                continue
+            if cmd_status != "active":
+                status = "error"
+                break
+        envs = common.load_envs(self.apprc)
+        url_data = {"host": envs.get("TSURU_HOST"),
+                    "appname": envs.get("TSURU_APPNAME"),
+                    "unitname": self.hostname}
+        url = "{host}/apps/{appname}/units/{unitname}"
+        token = envs.get("TSURU_APP_TOKEN")
+        requests.post(url.format(**url_data), data={"status": status},
+                      headers={"Authorization": "bearer " + token})
---
+++ tsuru/tests/plugins/test_status_reporter.py
@@ -0,0 +1,94 @@
+# Copyright 2014 tsuru-circus authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+import unittest
+
+import mock
+
+from tsuru.plugins import StatusReporter
+
+
+class StatusReporterTestCase(unittest.TestCase):
+
+    def test_init(self):
+        status_reporter = StatusReporter("", "", 1,
+                                         loop_rate=180,
+                                         apprc="/etc/apprc")
+        self.assertEqual(180, status_reporter.loop_rate)
+        self.assertEqual("/etc/apprc", status_reporter.apprc)
+
+    def test_init_default_config_values(self):
+        status_reporter = StatusReporter("", "", 1)
+        self.assertEqual(60, status_reporter.loop_rate)
+        self.assertEqual("/home/application/apprc", status_reporter.apprc)
+
+    def test_handle_init(self):
+        status_reporter = StatusReporter("", "", 1)
+        status_reporter.period.start = mock.Mock()
+        status_reporter.handle_init()
+        status_reporter.period.start.assert_called_once()
+
+    def test_handle_stop(self):
+        status_reporter = StatusReporter("", "", 1)
+        status_reporter.period.stop = mock.Mock()
+        status_reporter.handle_stop()
+        status_reporter.period.stop.assert_called_once()
+
+    @mock.patch("tsuru.common.load_envs")
+    @mock.patch("requests.post")
+    @mock.patch("tsuru.plugins.gethostname")
+    def test_report_ignores_plugins(self, gethostname, post, load_envs):
+        gethostname.return_value = "myhost"
+        status_reporter = StatusReporter("", "", 1)
+        load_envs.return_value = {"TSURU_HOST": "http://tsuru.io:8080",
+                                  "TSURU_APP_TOKEN": "abc123",
+                                  "TSURU_APPNAME": "something"}
+        call = mock.Mock()
+        call.return_value = {"statuses": {"plugin:tsuru-hooks": "stopped",
+                                          "something": "active"}}
+        status_reporter.call = call
+        status_reporter.report()
+        call.assert_called_once()
+        url = "http://tsuru.io:8080/apps/something/units/myhost"
+        post.assert_called_with(url, data={"status": "started"},
+                                headers={"Authorization": "bearer abc123"})
+
+    @mock.patch("tsuru.common.load_envs")
+    @mock.patch("requests.post")
+    @mock.patch("tsuru.plugins.gethostname")
+    def test_report_ignores_tsuru_hooks(self, gethostname, post, load_envs):
+        gethostname.return_value = "myhost"
+        status_reporter = StatusReporter("", "", 1)
+        load_envs.return_value = {"TSURU_HOST": "http://tsuru.io:8080",
+                                  "TSURU_APP_TOKEN": "abc123",
+                                  "TSURU_APPNAME": "something"}
+        call = mock.Mock()
+        call.return_value = {"statuses": {"tsuru-hooks": "stopped",
+                                          "something": "active"}}
+        status_reporter.call = call
+        status_reporter.report()
+        call.assert_called_once()
+        url = "http://tsuru.io:8080/apps/something/units/myhost"
+        post.assert_called_with(url, data={"status": "started"},
+                                headers={"Authorization": "bearer abc123"})
+
+    @mock.patch("tsuru.common.load_envs")
+    @mock.patch("requests.post")
+    @mock.patch("tsuru.plugins.gethostname")
+    def test_report_non_active_process(self, gethostname, post, load_envs):
+        gethostname.return_value = "myhost"
+        status_reporter = StatusReporter("", "", 1)
+        load_envs.return_value = {"TSURU_HOST": "http://tsuru.io:8080",
+                                  "TSURU_APP_TOKEN": "abc123",
+                                  "TSURU_APPNAME": "something"}
+        call = mock.Mock()
+        call.return_value = {"statuses": {"otherthing": "active",
+                                          "something": "active",
+                                          "wat": "stopped"}}
+        status_reporter.call = call
+        status_reporter.report()
+        call.assert_called_once()
+        url = "http://tsuru.io:8080/apps/something/units/myhost"
+        post.assert_called_with(url, data={"status": "error"},
+                                headers={"Authorization": "bearer abc123"})
