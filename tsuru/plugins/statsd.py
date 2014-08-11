3d4696f104eec2296e05db809fe29d4e45a76543
Andrews Medina <andrewsmedina@gmail.com>
2014-08-11 19:53:23 -0300

    plugins: statsd plugin.

---
+++ tsuru/plugins/statsd.py
@@ -0,0 +1,106 @@
+import os
+
+from zmq.eventloop import ioloop
+
+from circus.plugins import CircusPlugin
+from circus.util import human2bytes
+
+
+class StatsdEmitter(CircusPlugin):
+    name = 'statsd'
+    default_app_name = "app"
+
+    def __init__(self, endpoint, pubsub_endpoint, check_delay, ssh_server,
+                 **config):
+        super(StatsdEmitter, self).__init__(endpoint, pubsub_endpoint,
+                                            check_delay, ssh_server=ssh_server)
+        self.app = config.get('application_name', self.default_app_name)
+        # tsuru.app.host
+        app_name = os.environ.get("TSURU_APPNAME")
+        host_name = os.environ.get("HOSTNAME")
+        self.prefix = 'tsuru.{}.{}'.format(app_name, host_name)
+
+        # initialize statsd
+        from circus.plugins.statsd import StatsdClient
+        self.statsd = StatsdClient(
+            host=config.get('host', 'localhost'),
+            port=int(config.get('port', '8125')),
+            prefix=self.prefix,
+            sample_rate=float(config.get('sample_rate', '1.0'))
+        )
+
+    def handle_recv(self, data):
+        watcher_name, action, msg = self.split_data(data)
+        self.statsd.increment('{}.{}'.format(watcher_name, action))
+
+    def stop(self):
+        self.statsd.stop()
+        super(StatsdEmitter, self).stop()
+
+
+class BaseObserver(StatsdEmitter):
+
+    def __init__(self, *args, **config):
+        super(BaseObserver, self).__init__(*args, **config)
+        self.loop_rate = float(config.get("loop_rate", 60))  # in seconds
+
+    def handle_init(self):
+        self.period = ioloop.PeriodicCallback(self.look_after,
+                                              self.loop_rate * 1000, self.loop)
+        self.period.start()
+
+    def handle_stop(self):
+        self.period.stop()
+        self.statsd.stop()
+
+    def handle_recv(self, data):
+        pass
+
+    def look_after(self):
+        raise NotImplementedError()
+
+
+class Stats(BaseObserver):
+
+    name = 'stats'
+
+    def look_after(self):
+        info = self.call("stats")
+        if info["status"] == "error":
+            self.statsd.increment("_stats.error")
+            return
+
+        for name, stats in info['infos'].items():
+            if name.startswith("plugin:"):
+                # ignore plugins
+                continue
+
+            cpus = []
+            mems = []
+            mem_infos = []
+
+            for sub_name, sub_info in stats.items():
+                if isinstance(sub_info, dict):
+                    cpus.append(sub_info['cpu'])
+                    mems.append(sub_info['mem'])
+                    mem_infos.append(human2bytes(sub_info['mem_info1']))
+                elif sub_name == "spawn_count":
+                    # spawn_count info is in the same level as processes
+                    # dict infos, so if spawn_count is given, take it and
+                    # continue
+                    self.statsd.gauge("{}.spawn_count".format(name),
+                                      sub_info)
+
+                self.statsd.gauge("{}.watchers_num".format(name), len(cpus))
+
+                if not cpus:
+                    # if there are only dead processes, we have an empty list
+                    # and we can't measure it
+                    continue
+
+                self.statsd.gauge("{}.cpu_max".format(name), max(cpus))
+                self.statsd.gauge("{}.cpu_sum".format(name), sum(cpus))
+                self.statsd.gauge("{}.mem_pct_max".format(name), max(mems))
+                self.statsd.gauge("{}.mem_pct_sum".format(name), sum(mems))
+                self.statsd.gauge("{}.mem_max".format(name), max(mem_infos))
+                self.statsd.gauge("{}.mem_sum".format(name), sum(mem_infos))
---
+++ tsuru/tests/plugins/test_statsd.py
@@ -0,0 +1,51 @@
+import os
+
+from tornado.testing import gen_test
+
+from circus.tests.support import TestCircus, async_poll_for
+from circus.tests.support import async_run_plugin
+
+from tsuru.plugins.statsd import Stats, StatsdEmitter
+
+from mock import patch
+
+
+def get_gauges(queue, plugin):
+    queue.put(plugin.statsd.gauges)
+
+
+class TestStats(TestCircus):
+
+    @gen_test
+    def test_stats(self):
+        dummy_process = 'circus.tests.support.run_process'
+        yield self.start_arbiter(dummy_process)
+        async_poll_for(self.test_file, 'START')
+
+        config = {'loop_rate': 0.2}
+        gauges = yield async_run_plugin(
+            Stats, config,
+            plugin_info_callback=get_gauges,
+            duration=1000,
+            endpoint=self.arbiter.endpoint,
+            pubsub_endpoint=self.arbiter.pubsub_endpoint
+        )
+
+        # we should have a bunch of stats events here
+        self.assertTrue(len(gauges) >= 5)
+        last_batch = sorted(name for name, value in gauges[-5:])
+        wanted = ['test.cpu_sum', 'test.mem_max',
+                  'test.mem_pct_max', 'test.mem_pct_sum',
+                  'test.mem_sum']
+        self.assertEqual(last_batch, wanted)
+
+        yield self.stop_arbiter()
+
+    @patch("circus.plugins.statsd.StatsdClient")
+    def test_prefix(self, client_mock):
+        os.environ["HOSTNAME"] = "somehost"
+        os.environ["TSURU_APPNAME"] = "appname"
+        StatsdEmitter("endpoint", "pubsub", 1.0, "ssh_server")
+        client_mock.assert_called_with(host='localhost', sample_rate=1.0,
+                                       prefix='tsuru.appname.somehost',
+                                       port=8125)
