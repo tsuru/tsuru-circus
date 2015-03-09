2c28ec862341b538a53b1edc033f318de7c7115b
Andrews Medina <andrewsmedina@gmail.com>
2015-03-09 18:24:59 -0300

    remove duplicated license header.

--- tsuru/plugins/stats/statsd.py
+++ tsuru/plugins/stats/statsd.py
@@ -2,10 +2,6 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-# Copyright 2015 tsuru-circus authors. All rights reserved.
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
-
 import os
 import socket
 
